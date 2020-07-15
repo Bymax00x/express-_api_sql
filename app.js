cb26908a5e2edea6965001d58204228d7293aa8a
Bymax00x <bymaxmndr@gmail.com>
2020-07-15 20:59:42 +0545

    user api and all features

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const userRouter = require('./api/Users/user.router');
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 app.get('/', (req, res) => {
     res.json({
